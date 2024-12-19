feecaf5ef5fc98217f304b8ea9038fb0568ada8f
Stepcu <69991785+Stepcu@users.noreply.github.com>
2024-12-18 19:30:36 -0600

    Initial commint task manager

---
+++ controllers/taskControllers.js
@@ -0,0 +1,39 @@
+const Task = require("../models/Task");
+
+exports.getTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({ user: req.user.id });
+    res.status(200).json(tasks);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.createTask = async (req, res) => {
+  try {
+    const task = await Task.create({ ...req.body, user: req.user.id });
+    res.status(201).json(task);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+exports.updateTask = async (req, res) => {
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(200).json(task);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+exports.deleteTask = async (req, res) => {
+  try {
+    await Task.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "Task deleted" });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
